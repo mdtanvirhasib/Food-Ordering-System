9f8e024121f85e110b91372a27f43312f96f8625
Md. Tanvir Hasib <md.tanvirhasib11@gmail.com>
2025-12-31 10:26:24 +0600

    1

---
+++ User/View/userlogin.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="../Css/userlogin.css">
+    </head>
+
+    <body>
+        <img src="../Image/login.jpg" class="setimg" alt="">
+        <form action="">
+            <h1>User Login</h1>
+     
+                        <label for="">Email ID:</label>
+     
+                        <input type="text" placeholder="Enter Email ID" id="in">
+
+                        <label for="">Password:</label>
+
+                        <input type="password" placeholder="Enter Password" id="in">
+
+                        <a href="" id="fp">Forgot Password?</a>
+            <input type="submit" value="Login" id="sub">
+            <a href="../View/userreg.php" id="reg">Create Account</a>
+
+        </form>
+    </body>
+</html>
---
+++ User/View/userreg.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="../Css/userreg.css">
+    </head>
+
+    <body>
+        <img src="../Image/registration.jpg" class="setimg" alt="">
+        <form action="">
+            <h1>Registration Form</h1>
+            <label for="">Name:</label>
+            <input type="text" placeholder="Enter Your Name" id="in">
+
+            <label for="">Address:</label>
+            <input type="text" placeholder="Enter Your Adderss" id=in>
+
+            <label for="">Mobile Number:</label>
+            <input type="tel" placeholder="Enter Mobile Number" id="in">
+
+            <label for="">Email:</label>
+            <input type="email" placeholder="Enter Email Address" id="in">
+
+            <label for="">Password:</label>
+            <input type="password" placeholder="Enter Password" id="in">
+            <label for="">Comfirm Password:</label>
+            <input type="password" placeholder="Enter Password" id="in">
+
+            <input type="submit" value="Register" id="reg">
+
+            <a href="../View/userlogin.php" id="li">Already have an account?</a>
+        </form>
+    </body>
+</html>
