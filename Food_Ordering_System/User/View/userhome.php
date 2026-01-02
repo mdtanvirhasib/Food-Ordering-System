b61b535752ce61a647aa18b62d88d073776e97fe
Md. Tanvir Hasib <md.tanvirhasib11@gmail.com>
2026-01-02 19:57:10 +0600

    5

--- Food_Ordering_System/User/View/userhome.php
+++ Food_Ordering_System/User/View/userhome.php
@@ -1,3 +1,8 @@
+<?php
+include "../DB/db.php";
+$result=mysqli_query($conn,"SELECT* FROM food");
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -7,9 +12,16 @@
 
         <body>
             <h2>Food Menu</h2>
-            <form action="">
-                
+            <?php while($a=mysqli_fetch_assoc($result)){?>
+            <form method="post">
+                <img src="../uploads/" alt="">
+                <h3><?= $a['name']?></h3>
+                <p>TK<?= $a['price']?></p>
+                <input type="hidden" <?php $a['id']?>>
+
+
             </form>
+            <?php }?>
         </body>
     </head>
 </html>
