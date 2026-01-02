81bfd2e21835ddc626fb98ec96de045d4334ca75
Md. Tanvir Hasib <md.tanvirhasib11@gmail.com>
2026-01-03 00:50:48 +0600

    6

--- Food_Ordering_System/User/View/userhome.php
+++ Food_Ordering_System/User/View/userhome.php
@@ -1,5 +1,5 @@
 <?php
-include "../DB/db.php";
+include "../../Admin/DB/db.php";
 $result=mysqli_query($conn,"SELECT* FROM food");
 ?>
 
@@ -9,19 +9,20 @@ $result=mysqli_query($conn,"SELECT* FROM food");
         <title>
             Home
         </title>
-
+        <link rel="stylesheet" href="../Css/userhome.css">
+    </head>
         <body>
             <h2>Food Menu</h2>
             <?php while($a=mysqli_fetch_assoc($result)){?>
-            <form method="post">
-                <img src="../uploads/" alt="">
+            <form method="post" class="viewfood">
+                <img src="../../Admin/uploads/<?=$a['image']?>" alt="">
                 <h3><?= $a['name']?></h3>
                 <p>TK<?= $a['price']?></p>
-                <input type="hidden" <?php $a['id']?>>
+                <input type="hidden" name="id" value="<?= $a['id']?>">
 
 
             </form>
             <?php }?>
         </body>
-    </head>
+    
 </html>
