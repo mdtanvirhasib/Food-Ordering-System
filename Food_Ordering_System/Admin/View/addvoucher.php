49b44ba8b9c3d58031b86bf191088749bda1fed0
Md. Tanvir Hasib <md.tanvirhasib11@gmail.com>
2026-01-12 12:51:27 +0600

    12

---
+++ Food_Ordering_System/Admin/View/addvoucher.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Add vaucher</title>
+    </head>
+
+    <body>
+        <form action="" method="post">
+            <h2>Add vaucher</h2>
+            <label for="">Code:</label>
+            <input type="text" name="code">
+            <label for="">Discount:</label>
+            <input type="text" name="discount">
+            <button name="add">Generate</button>
+        </form>
+    </body>
+</html>
