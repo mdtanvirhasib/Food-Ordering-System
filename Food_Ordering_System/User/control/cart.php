9ff8bc48d938c7b1c071f675ea701d38b2f3d344
Md. Tanvir Hasib <md.tanvirhasib11@gmail.com>
2026-01-13 18:33:08 +0600

    15

--- Food_Ordering_System/User/control/cart.php
+++ Food_Ordering_System/User/control/cart.php
@@ -13,6 +13,21 @@ if($_SERVER['REQUEST_METHOD']=='POST')
     {
         $id=$_POST['id'];
         $q=mysqli_query($conn,"SELECT * FROM food WHERE id='$id");
+        if($q && mysqli_num_rows($q)>0)
+            {
+                $food=mysqli_fetch_assoc($q);
+            }
+            $found = false;
+            foreach ($_SESSION['cart'] as &$item) 
+                {
+                    if ($item['id'] == $food['id']) 
+                        {
+                            $item['quantity']++;
+                            $found = true;
+                            break;
+                        }
+                }
+                unset($item);
 
     }
 ?>
