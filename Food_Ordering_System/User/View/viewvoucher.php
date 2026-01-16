f3145a29469930d9721d316b98d227b4213bf834
Md. Tanvir Hasib <md.tanvirhasib11@gmail.com>
2026-01-17 00:45:21 +0600

    34

--- Food_Ordering_System/User/View/viewvoucher.php
+++ Food_Ordering_System/User/View/viewvoucher.php
@@ -1,27 +1,31 @@
 <?php
 include "../DB/db.php";
-$result=mysqli_query($conn,"SELECT * FROM voucher");
+$result = mysqli_query($conn, "SELECT * FROM voucher");
 
 ?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>My voucher</title>
-        <link rel="stylesheet" href="../Css/viewvoucher.css">
-    </head>
-    <body>
-        <h2>Vouchers</h2>
-        <?php while($a=mysqli_fetch_assoc($result)){?>
-        <form action="">
-            <p>Code: <?= $a['code']?></p>
-            <p>Discount: <?= $a['discount']?>%</p>
+
+<head>
+    <title>My voucher</title>
+    <link rel="stylesheet" href="../Css/viewvoucher.css">
+</head>
+
+<body>
+    <form action="" id="links">
+        <a href="../View/userhome.php">Home</a>
+        <a href="../View/cart.php">My cart</a>
+        <a href="../View/viewvoucher.php">Voucher</a>
+        <a href="../control/logout.php" class="logout">Logout</a>
+    </form>
+    <h2>Vouchers</h2>
+    <?php while ($a = mysqli_fetch_assoc($result)) { ?>
+        <form action="" class="vaucher">
+            <p>Code: <?= $a['code'] ?></p>
+            <p>Discount: <?= $a['discount'] ?>%</p>
         </form>
-        <?php }?>
-                    <form action="" id="links">
-                <a href="../View/userhome.php">Home</a>
-                <a href="../View/cart.php">My cart</a>
-                <a href="../View/viewvoucher.php">Voucher</a>
-                <a href="../control/logout.php" class="logout">Logout</a>
-            </form>
-    </body>
+    <?php } ?>
+
+</body>
+
 </html>
