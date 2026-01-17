889454399b560dfa5387785074fd8cd19bcb76e6
Md. Tanvir Hasib <md.tanvirhasib11@gmail.com>
2026-01-17 13:52:05 +0600

    44

---
+++ Food_Ordering_System/Admin/Controller/adminlogin.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+include "../DB/db.php";
+
+$error="";
+if($_SERVER['REQUEST_METHOD']=='POST')
+    {
+        $email=$_POST['email'];
+        $password=$_POST['password'];
+    }
+
+    if(empty($email)||empty($password))
+        {
+            $error="Email and password can't be empty";
+        }
+
+        else
+            {
+                $sql="SELECT * FROM users where email='$email' and password ='$password' and role='admin'";
+
+                $res=$conn->query($sql);
+
+                if($res->num_rows==1)
+                    {
+                        $user=mysqli_fetch_assoc($res);
+
+                        $_SESSION['user']=$user['id'];
+                        $_SESSION['email'] = $user['email'];
+
+                        header("Location: ../View/adminhome.php");
+                        exit;
+                    }
+                    else
+                        {
+                            $error = "Invalid Email or Password";
+                        }
+
+            }
+
+?>
