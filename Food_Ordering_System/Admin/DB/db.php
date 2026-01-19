ae4b0288245be7da8629cd3e58474a6d79dedbb7
Md. Tanvir Hasib <md.tanvirhasib11@gmail.com>
2026-01-19 20:06:59 +0600

    48

--- Food_Ordering_System/Admin/DB/db.php
+++ Food_Ordering_System/Admin/DB/db.php
@@ -2,7 +2,7 @@
 $host="localhost";
 $user="root";
 $pass="";
-$dbname="foodhub";
+$dbname="foodhubs";
 $conn=new
 mysqli($host,$user,$pass,$dbname);
 if ($conn->connect_error)
--- Food_Ordering_System/Admin/View/adminhome.php
+++ Food_Ordering_System/Admin/View/adminhome.php
@@ -1,1 +1,16 @@
-Hello
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            Admin Home
+        </title>
+    </head>
+    <body>
+        <form action="">
+            <a href="../View/adminhome.php">Home</a>
+            <a href="../View/addfood.php">Add food</a>
+            <a href="../View//addvoucher.php">Add voucher</a>
+            
+        </form>
+    </body>
+</html>
